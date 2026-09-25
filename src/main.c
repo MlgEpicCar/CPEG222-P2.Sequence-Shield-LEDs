59dadd0b7c68f2c7efae8120dc4a81b05f1ab5cc
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2026-09-24 20:41:52 -0400

    made D1-4 activate from switches

--- src/main.c
+++ src/main.c
@@ -6,32 +6,29 @@
 #include "stm32f4xx.h"
 #include <stdbool.h>
 
-/* Refers to the blue button on the board
-IMPORTANT: USER_BUTTON_PIN will normally be LOW but then become HIGH when pressed 
-(bc of step-up or smth)*/
-#define BUTTON_PIN 13
-#define BUTTON_PORT GPIOC
+/* Button Definitions */
+#define LEFT_PIN 9 // GPIOF
+#define RIGHT_PIN 6 // NOTE: Right uses GPIOE (for some fucking reason)
+#define CENTER_PIN 8 // also GPIOF
 
-/* Unlike the NUCLEO-F446ZE onboard LEDs, the CPEG222 Shield LEDs are PD0-7 */
-#define GREEN_LED_PIN  0
-#define BLUE_LED_PIN   7
-#define RED_LED_PIN    14
+/* Switch Definitions */
+#define S1 8
+#define S2 9
+#define S3 10
+#define S4 11
+#define SWITCH_PORT GPIOC
+
+/* Unlike the NUCLEO-F446ZE onboard LEDs, the CPEG222 Shield LEDs are PD0-7 (making it easy)*/
 #define LED_PORT GPIOD
 
-volatile bool button_pressed = false;
 volatile uint32_t button_debounce = 0;
 
 void SysTick_Handler(void) {
-    /* Button is HIGH when pressed */
-    if (BUTTON_PORT->IDR & (1U << BUTTON_PIN))
+    /* SUPER AWESOME DEBOUNCE TIME */
+    if (button_debounce == 0)
     {
-        /* SUPER AWESOME DEBOUNCE TIME */
-        if (button_debounce == 0)
-        {
-            button_pressed = true;
-            /* 67 ms debounce/cooldown */
-            button_debounce = 67;
-        }
+        /* 100 ms debounce/cooldown */
+        button_debounce = 100;
     }
 
     /* Decrease cooldown every millisecond */
@@ -39,109 +36,76 @@ void SysTick_Handler(void) {
     {
         button_debounce--;
     }
-
-    /* Prevent holding from constantly switching colors */
-    while (BUTTON_PORT->IDR & (1U << BUTTON_PIN))
-    {
-        // Do nothing
-    }
 }
 
 int main(void)
 {
-    /* Enable clock for GPIOB and GPIOC */
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN |
-                     RCC_AHB1ENR_GPIOCEN;
+    /* Enable clock for GPIOC, GPIOD, GPIOE, and GPIOF */
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN |
+                     RCC_AHB1ENR_GPIODEN|
+                     RCC_AHB1ENR_GPIOEEN|
+                     RCC_AHB1ENR_GPIOFEN;
 
-    /* Bit Manip Magic Breakdown (kinda just notes for my future self ngl)
-    *  Goal: We need to edit GPIOB so that we can set our pins mode to output
-    * 
-    *  Step 1: Start with the onboard LEDs and their port numbers as an example we can use
-    *  the blue LED whose port number is 7, in MODER since each pin gets 2 digits this means we
-    *  can simply multiply the port number by 2 to get it's designated spot (14 and 15). Another
-    *  way to think about it is to realize port num 0 gets digits 0 and 1, port num 1 gets 
-    *  digits 2 and 3... regardless of it's port number, multiplying it by the number of digits 
-    *  the value you're aiming for is will always lead to what you're looking for
-    * 
-    *  Step 2: We use 3U to set the value at the two important bits to 11, 3 in binary is 11 and
-    *  the U means it's unsigned (not able to be negative, think abosulte value)
-    * 
-    *  Step 3: We use ~ to flip all of the bits, so 00000000011000000 becomes 11111110011111
-    *  we do this to prepare to AND our mask with the original
-    * 
-    *  Step 4: We use x &= y (a short hand of x = x & y) to and our original dataset with
-    *  our mask so that anything that was originally a 0 stays a 0 and anything that was
-    *  a 1 stays a 1, but critically our two bits become 00 regardless of what they were before
-    */
-
-    /* Clear PB0, PB7, PB14 so we can set them to general purpose output */
+    /* Clear PD0-7 so we can set them to general purpose output */
     LED_PORT->MODER &= ~((3U << (0 * 2)) |
-                       (3U << (1 * 2)) |
-                       (3U << (2 * 2)) |
-                       (3U << (3 * 2)) |
-                       (3U << (4 * 2)) |
-                       (3U << (5 * 2)) |
-                       (3U << (6 * 2)) |
-                       (3U << (7 * 2)));
-    /* Set PB0, PB7, PB14 as general purpose output (01) */
-    LED_PORT->MODER |= (1U << (0 * 2)) |
-                    (1U << (1 * 2)) |
-                    (1U << (2 * 2)) |
-                    (1U << (3 * 2)) |
-                    (1U << (4 * 2)) |
-                    (1U << (5 * 2)) |
-                    (1U << (6 * 2)) |
+                        (3U << (1 * 2)) |
+                        (3U << (2 * 2)) |
+                        (3U << (3 * 2)) |
+                        (3U << (4 * 2)) |
+                        (3U << (5 * 2)) |
+                        (3U << (6 * 2)) |
+                        (3U << (7 * 2)));
+    /* Set PD0-7 as general purpose output (01) */
+    LED_PORT->MODER |= (1U << (0 * 2))|
+                    (1U << (1 * 2))|
+                    (1U << (2 * 2))|
+                    (1U << (3 * 2))|
+                    (1U << (4 * 2))|
+                    (1U << (5 * 2))|
+                    (1U << (6 * 2))|
                     (1U << (7 * 2));
 
-    /* Similar to GPIOB, BSRR stores values for our LEDs. BSRR controls if it is on or not,
-    however, there is a critial difference, for BSRR the first 16 bits (0-15) would be set to 1
-    whenever you want to turn something on, and that same value +16 (16-31) would be set to 1 to
-    turn the same thing off. In other words BSRR is write only */
-
     /* Ensure all LEDs start off */
-    LED_PORT->BSRR = (1U << (0 + 16)) |
-                  (1U << (1 + 16)) |
-                  (1U << (2 + 16)) |
-                  (1U << (3 + 16)) |
-                  (1U << (4 + 16)) |
-                  (1U << (5 + 16)) |
-                  (1U << (6 + 16)) |
+    LED_PORT->BSRR = (1U << (0 + 16))|
+                  (1U << (1 + 16))|
+                  (1U << (2 + 16))|
+                  (1U << (3 + 16))|
+                  (1U << (4 + 16))|
+                  (1U << (5 + 16))|
+                  (1U << (6 + 16))|
                   (1U << (7 + 16));
 
-    /* bit manip for the button is a lot easier since it's an input (00)*/
-    /* Configure Button */
-    BUTTON_PORT->MODER &= ~(3U << (BUTTON_PIN * 2));
+    /* Configure Buttons */
+    GPIOF->MODER &= ~((3U << (LEFT_PIN * 2))|
+                     (3U << (CENTER_PIN * 2)));  
+    GPIOE->MODER &= ~(3U << (RIGHT_PIN * 2)); // RIGHT uses it's own GPIO port 
+
+    /* Configure Switches */
+    SWITCH_PORT->MODER &= ~((3U << (S1 * 2))|
+                     (3U << (S2 * 2))|
+                     (3U << (S3 * 2))|
+                     (3U << (S4 * 2))); 
 
     /* Configure SysTick*/
     SysTick_Config(SystemCoreClock / 1000);
 
-    /*List of LED addresses to sift through*/
-    const uint8_t leds[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-
-    /* Always start with Red LED*/
-    int current_led = 0;
-    LED_PORT->BSRR = (1U << leds[current_led]);
-
     while (1)
     {
-        if (button_pressed)
-        {
-            /* Clear the flag */
-            button_pressed = false;
+        /* Turn on/off LEDs when switches are #switched */
+        if (SWITCH_PORT->IDR & (1U << S1)) { // SWITCH 1 & D1(LED 1)
+            LED_PORT->BSRR = (1U << 0); } else {
+            LED_PORT->BSRR = (1U << (0 + 16)); }
 
-            /* Turn current LED off */
-            LED_PORT->BSRR = (1U << (leds[current_led] + 16));
+        if (SWITCH_PORT->IDR & (1U << S2)) { // SWITCH 2 & D2(LED 2)
+            LED_PORT->BSRR = (1U << 1); } else {
+            LED_PORT->BSRR = (1U << (1 + 16)); }
 
-            /* Move to next LED */
-            current_led++;
+        if (SWITCH_PORT->IDR & (1U << S3)) { // SWITCH 3 & D3
+            LED_PORT->BSRR = (1U << 2); } else {
+            LED_PORT->BSRR = (1U << (2 + 16)); }
 
-            if (current_led >= 8)
-            {
-                current_led = 0;
-            }
-
-            /* Turn next LED on */
-            LED_PORT->BSRR = (1U << leds[current_led]);
-        }
+        if (SWITCH_PORT->IDR & (1U << S4)) { // SWITCH 4 & D4
+            LED_PORT->BSRR = (1U << 3); } else {
+            LED_PORT->BSRR = (1U << (3 + 16)); }
     }
 }
